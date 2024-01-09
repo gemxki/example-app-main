002b921a57d3c8757a8aa3e80171b7f53536ee94
gemxki <152680921+gemxki@users.noreply.github.com>
2024-01-09 12:42:26 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller {
-    
+
     public function register(Request $request){
 
         $attrs = $request->validate([
@@ -25,27 +25,23 @@ class AuthController extends Controller {
         return response([
             'response' => 'success'
         ], 200);
-       
+
     }
 
-    public function login(Request $request){
-        
-        $attrs = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|min:6'
-        ]);
-
-        if(!Auth::attempt($attrs)){
-            return Response([
-                'message' => 'Invalid credentials.'
-            ], 403);
-        }
-
-        return response([
-            'user' => auth()->user(),
-            'token' => auth()->user()->createToken('secret')->plainTextToken
-        ], 200);
-        
+    public function login(Request $request)
+{
+    $login = $request->validate([
+        'email' => 'required',
+        'password'=> 'required',
+    ]);
+    if (!Auth::attempt($login)){
+        return response()->json(['message' => 'error']);
     }
+    /** @var \App\Models\MyUserModel $user **/
+    $user = Auth::user();
+    $token = $user->createToken('Token Name')->plainTextToken;
+    return response()->json(['user' => $user, 'token' => $token]);
+}
 
 }
+
